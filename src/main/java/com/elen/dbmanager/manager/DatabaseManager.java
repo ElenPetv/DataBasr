533fe928ae6cf37cfa2855f2ac43bc18f8b3deca
Naik <naiksav4enko@gmail.com>
2021-12-14 14:43:50 +0200

    Allow comma, line breaks #15

--- src/main/java/com/elen/dbmanager/manager/DatabaseManager.java
+++ src/main/java/com/elen/dbmanager/manager/DatabaseManager.java
@@ -4,10 +4,14 @@ import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.subjects.BehaviorSubject;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.*;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static java.lang.System.out;
 
@@ -18,6 +22,10 @@ public class DatabaseManager {
     public static DatabaseManager getInstance() {
         return INSTANCE;
     }
+
+    private final Pattern valuePattern = Pattern.compile(
+            "(?m)(?<=^|,)(?:\"{2}|(?:)|[^,\"\r\n]+|\"(?:\"{2}|[^\"]+)+\")(?=,|$)"
+    );
 
     private final BehaviorSubject<List<List<String>>> dataSubject = BehaviorSubject.createDefault(List.of());
     private final BehaviorSubject<Optional<File>> fileSubject = BehaviorSubject.createDefault(Optional.empty());
@@ -35,32 +43,38 @@ public class DatabaseManager {
         File file = fileSubject.getValue()
                 .orElseThrow(() -> new NullPointerException("Can't read before open or create new file"));
         try {
-            Scanner sc = new Scanner(file).useDelimiter("(\r)?\n");
+            String fileContent = Files.readString(file.toPath());
             List<List<String>> data = new ArrayList<>();
             int targetRowSize = -1;
-            while (sc.hasNext()) {
-                String[] rowElements = sc.next().split(",", -1);
-                int rowSize = rowElements.length;
-                if (targetRowSize != -1) {
-                    if (rowSize > targetRowSize) {
-                        throw new IllegalArgumentException("Строка '" + Arrays.toString(rowElements) +
-                                "' содержит больше значений, чем количество столбцов!");
-                    } else if (rowSize < targetRowSize) {
-                        throw new IllegalArgumentException("Строка '" + Arrays.toString(rowElements) +
-                                "' содержит меньше значений, чем количество столбцов!");
-                    }
-                } else {
-                    targetRowSize = rowSize;
-                }
+            List<String> rowElements = new ArrayList<>();
+            Matcher matcher = valuePattern.matcher(fileContent);
+            while (matcher.find()) {
+                rowElements.add(decodeValue(matcher.group()));
+                if (fileContent.length() == matcher.end() || fileContent.charAt(matcher.end()) == '\n') {
 
-                data.add(new ArrayList<>(List.of(rowElements)));
+                    int rowSize = rowElements.size();
+                    if (targetRowSize != -1) {
+                        if (rowSize > targetRowSize) {
+                            throw new IllegalArgumentException("Строка '" + rowElements +
+                                    "' содержит больше значений, чем количество столбцов!");
+                        } else if (rowSize < targetRowSize) {
+                            throw new IllegalArgumentException("Строка '" + rowElements +
+                                    "' содержит меньше значений, чем количество столбцов!");
+                        }
+                    } else {
+                        targetRowSize = rowSize;
+                    }
+
+                    data.add(new ArrayList<>(rowElements));
+                    rowElements.clear();
+                }
             }
             if (data.isEmpty()) {
                 data.add(new ArrayList<>());
             }
             dataSubject.onNext(data);
-        } catch (FileNotFoundException e) {
-            out.println("Файл " + file.getAbsolutePath() + " не найден!");
+        } catch (IOException e) {
+            out.println("Ошибка чтения файла " + file.getAbsolutePath());
             e.printStackTrace();
         }
 
@@ -88,7 +102,10 @@ public class DatabaseManager {
                 .orElseThrow(() -> new NullPointerException("Cant call save before open or create new file"));
 
         String stringData = data.stream()
-                .map(rows -> rows.stream().reduce((s1, s2) -> s1 + "," + s2).orElse(""))
+                .map(rows -> rows.stream()
+                        .map(this::encodeValue)
+                        .reduce((s1, s2) -> s1 + "," + s2)
+                        .orElse(""))
                 .reduce((s, row) -> s + "\n" + row)
                 .orElse("");
         FileWriter writer = new FileWriter(file, false);
@@ -99,5 +116,23 @@ public class DatabaseManager {
 
     public List<List<String>> getData() {
         return dataSubject.getValue();
+    }
+
+    private String encodeValue(String value) {
+        String escaped = value.replaceAll("(?s)\"", "\"\"");
+        if (escaped.contains(",") || escaped.contains("\n") || escaped.contains("\"\"")) {
+            return "\"" + escaped + "\"";
+        } else {
+            return escaped;
+        }
+    }
+
+    private String decodeValue(String value) {
+        final String unescaped = value.length() > 2 ? value.replaceAll("(?s)\"\"", "\"") : value;
+        if (unescaped.length() > 1 && unescaped.startsWith("\"") && unescaped.endsWith("\"")) {
+            return unescaped.substring(1, unescaped.length() - 1);
+        } else {
+            return unescaped;
+        }
     }
 }
